b51e0502b25744c8622f3317b0764f5302ae3ec4
grossmannA <agrossmann315@gmail.com>
2024-01-17 23:58:48 -0500

    updated code

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,13 @@
 var startButtonEl = $('#start-btn');
 
 
-startButtonEl.on('click', function () {})
+startButtonEl.on('click', function () {
+    starttimer();
+})
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl
+    })
+}
